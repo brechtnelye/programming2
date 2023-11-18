43c2030e4791ea6c05f25e54ce1769d070eed19f
brecht <brecht.vaneylen@student.ucll.be>
2023-11-18 19:28:01 +0100

    wip lists

--- 01-basic-python/04-lists/05-median/student.py
+++ 01-basic-python/04-lists/05-median/student.py
@@ -1,1 +1,6 @@
-# Write your code here
+def median(ns):
+    ns = sorted(ns)
+    if len(ns) % 2 == 0:
+        return (ns[len(ns) // 2 - 1] + ns[len(ns) // 2]) / 2        
+    else:
+        return ns[len(ns) // 2]
